53286e0909858c64616bc2c336ba3e3db3948f41
cisternashdiego <37970600+cisternashdiego@users.noreply.github.com>
2018-05-11 00:46:53 -0300

    casi termianda la tarea

--- T1FACTS/TCSC.m
+++ T1FACTS/TCSC.m
@@ -2,12 +2,12 @@
 Ksmin=0.15; lam=2.1 ; Ksop=0.25; L=20e-3; XL=L*100*pi; V=1100*sqrt(2);
 syms x;
 %b0=pi/(2*lam); bloq=pi; no es necesario
-C0=1/(100*pi*Ksmin*XL);
-L0=1/(lam^2*(100*pi)^2*C0);
+C0=1/(100*pi*Ksmin*XL)
+L0=1/(lam^2*(100*pi)^2*C0)
 XCeq=Ksop*XL;
 Ceq=1/(100*pi*XCeq);
-Kbpu= C0/Ceq-1;
-bet= vpasolve(Kbpu== 2*lam^2/(pi*(lam^2-1))*(2*cos(x)^2/(lam^2-1)*(lam*tan(lam*x))-tan(x)) - x - sin(2*x)/2,x,[0 pi]); %beta en rad
+Kbpu= C0/Ceq-1
+bet= vpasolve(Kbpu== 2*lam^2/(pi*(lam^2-1))*(2*cos(x)^2/(lam^2-1)*(lam*tan(lam*x))-tan(x)) - x - sin(2*x)/2,x,[0 pi/4]) %beta en rad
 %ALPHA CON CORRIENTE SINTETIZADA
 del=-25*pi/180; %delta en radianes
 I=(V-V*(cos(del)+i*sin(del)))/(XL*(1-Ksop)); %corriente con compensacion
